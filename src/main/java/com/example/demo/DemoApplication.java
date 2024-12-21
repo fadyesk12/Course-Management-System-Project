5b0235ba6236c1d5f6b84b0d66229dd8e5895dc1
fadyesk12 <136813453+fadyesk12@users.noreply.github.com>
2024-12-21 18:16:52 +0200

    updated app file to initialize storage each time it runs

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import com.example.demo.Storage.StorageProperties;
 import com.example.demo.Storage.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
