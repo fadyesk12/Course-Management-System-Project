11b1c0b397e4f190e6cec12b9c575ec95bcd17ab
fadyesk12 <136813453+fadyesk12@users.noreply.github.com>
2024-12-21 12:09:35 +0200

    fixed issues with User.java

--- src/main/java/com/example/demo/Model/User.java
+++ src/main/java/com/example/demo/Model/User.java
@@ -1,32 +1,5 @@
 package com.example.demo.Model;
 
-<<<<<<< HEAD
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-@Getter
-@Setter
-@Entity
-@Table(name = "users")
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    protected Long id;
-
-    @Column(nullable = false, unique = true)
-    protected String name;
-
-    @Column(nullable = false)
-    protected String email;
-
-    @Column(nullable = false)
-    protected String password;
-
-    public User() {
-    }
-=======
 public abstract class User {
     private Long id;
     private String name;
@@ -34,7 +7,6 @@ public abstract class User {
     private String password;
 
     public User() {}
->>>>>>> 987585c7f22c2279dded27947a9273ac9e1ca2e4
 
     public User(Long id, String name, String email, String password) {
         this.id = id;
@@ -42,28 +14,10 @@ public abstract class User {
         this.email = email;
         this.password = password;
     }
-<<<<<<< HEAD
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-=======
     public User( String name, String email, String password) {
         this.name = name;
         this.email = email;
         this.password = password;
->>>>>>> 987585c7f22c2279dded27947a9273ac9e1ca2e4
     }
 
     public Long getId() {
@@ -82,10 +36,7 @@ public abstract class User {
         return password;
     }
 
-<<<<<<< HEAD
-=======
     public abstract String getRole();
 
     public abstract String toString();
->>>>>>> 987585c7f22c2279dded27947a9273ac9e1ca2e4
 }
