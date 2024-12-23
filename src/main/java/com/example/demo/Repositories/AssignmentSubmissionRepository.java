81f7c83b4f01702d88779b393117cdf3d8572d6a
KMicheal70 <125496077+KMicheal70@users.noreply.github.com>
2024-12-23 20:50:42 +0200

    findbyId

--- src/main/java/com/example/demo/Repositories/AssignmentSubmissionRepository.java
+++ src/main/java/com/example/demo/Repositories/AssignmentSubmissionRepository.java
@@ -1,10 +1,13 @@
 package com.example.demo.Repositories;
 
 import com.example.demo.Model.AssignmentSubmission;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AssignmentSubmissionRepository extends JpaRepository<AssignmentSubmission, Long> {
-    
+    Optional<AssignmentSubmission> findByStudentId(Long studentId);
 }
