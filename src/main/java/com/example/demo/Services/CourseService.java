cb23768bbba2450d292a6985776857e89017ecd3
Mohammed Zeinhom <mohamedzenhom200@gmail.com>
2024-12-23 18:16:44 +0200

    add lesson

--- src/main/java/com/example/demo/Services/CourseService.java
+++ src/main/java/com/example/demo/Services/CourseService.java
@@ -3,33 +3,37 @@ package com.example.demo.Services;
 import java.util.List;
 // import java.util.Optional;
 
+import com.example.demo.Model.*;
+import com.example.demo.Repositories.LessonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.Model.Course;
-import com.example.demo.Model.Student;
-import com.example.demo.Model.Lesson;
 import com.example.demo.Repositories.CourseRepository;
 
 @Service
 public class CourseService {
     private final CourseRepository courseRepository;
+    private final LessonRepository lessonRepository;
 
     @Autowired
-    public CourseService(CourseRepository courseRepository) {
+    public CourseService(CourseRepository courseRepository, LessonRepository lessonRepository) {
         this.courseRepository = courseRepository;
+        this.lessonRepository = lessonRepository;
     }
 
 
 
-//    public void AddLesson(String courseID, Lesson lesson){
-//        Course course = courseRepository.findByID(courseID);
-//        if (course == null) {
-//            throw new IllegalStateException("Course with this ID doesn't exist.");
-//        }
-//        course.AddLesson(lesson);
-//        courseRepository.save(course);
-//    }
+    public Lesson AddLesson(Long courseId, Lesson lesson){
+        Course course = courseRepository.findById(courseId).orElseThrow(() -> new RuntimeException("Course not found with id: " + courseId)); // making sure the course already exists
+        if (lessonRepository.existsById(lesson.getId())){
+            throw new IllegalStateException("Lesson already added");
+        }
+        lesson.setCourse(course);
+        course.getLessons().add(lesson);
+        courseRepository.save(course);
+
+        return lesson;
+    }
     public List<Student> getStudentList(Course course){
         return course.getEnrolledStudents().stream().toList();
     }
